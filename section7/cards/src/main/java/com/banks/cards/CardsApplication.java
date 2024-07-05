80e5e3ebaf7b377c32701d2852afba9ffcfc459c
syam1310 <psyam0175@gmail.com>
2024-07-05 20:06:08 +0530

    Adding mysql db from the docker for three services

---
+++ section7/cards/src/main/java/com/banks/cards/CardsApplication.java
@@ -0,0 +1,42 @@
+package com.banks.cards;
+
+import com.banks.cards.dto.CardsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.bank.cards.controller") })
+@EnableJpaRepositories("com.bank.cards.repository")
+@EntityScan("com.bank.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Bank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Shyam",
+						email = "syam@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Cards microservice REST API Documentation"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
